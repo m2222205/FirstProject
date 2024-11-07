757f3b584d8996008c42bdd0a951ceff7a638099
m2222205 <abduhalilovamaftuna1@gmail.com>
2024-11-07 12:04:31 +0500

    changed

--- src/src/Program.cs
+++ src/src/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("4 xonali son kirit");
+            var number = Convert.ToInt32(Console.ReadLine());
+            var digit1 = number % 10;
+            var digit2 = number / 10 % 10;
+            var digit3 = number / 100 % 10;
+            var digit4 = number / 1000 % 10;
+            var result = digit1 + digit2 + digit3 + digit4;
+            Console.WriteLine(result);
+
+
 
 
         }
--- src/src/obj/Debug/net8.0/src.AssemblyInfo.cs
+++ src/src/obj/Debug/net8.0/src.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5e25f45906489c91259b3d808cd3f42628adce3")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
